24eaea91331d2c3d2d3f47448b6a3bc0186b95ad
Fabio <fabioherlop@gmail.com>
2019-09-23 15:25:39 +0200

    Initial commit

---
+++ app/src/main/java/es/upv/gnd/primeraclase/MainActivity.java
@@ -0,0 +1,29 @@
+package es.upv.gnd.primeraclase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity { //clase siempre publica para que la lea android
+
+    /*//@Override  (opcional)
+    protected void onCreate(Bundle savedInstanceState) {//cuando esta actividad sea creada,se llama a la funcion
+        Log.d("HolaMundo","Entramos en onCreate");
+        super.onCreate(savedInstanceState);
+        Object o = null;
+        //o.toString();
+        setContentView(R.layout.activity_main);
+    }*/
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        TextView texto = new TextView(this);
+        texto.setText("Hello, Android");
+        texto.setTextColor(Color.RED); //color de las letras
+        setContentView(texto);
+    }
+}
